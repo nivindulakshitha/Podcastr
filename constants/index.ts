b9f48ef9e3b9087e3b1a2eecf5ab96a3abf61a4f
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-10 17:57:03 +0530

    Add sidebarItems constant for navigation menu

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarItems = [
+	{
+		label: "Home",
+		route: "/",
+		imgURL: "/icons/home.svg",
+	},
+	{
+		label: "Discover",
+		route: "/discover",
+		imgURL: "/icons/discover.svg",
+	},
+	{
+		label: "Create",
+		route: "/create-podcast",
+		imgURL: "/icons/microphone.svg",
+	},
+	{
+		label: "Profile",
+		route: "/profile",
+		imgURL: "/icons/profile.svg",
+	}
+];
