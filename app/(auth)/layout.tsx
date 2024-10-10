247f74358453958535ba8603abd384e90da58b28
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-10 17:13:54 +0530

    Add new layout component for authentication pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<main>
+			{children}
+		</main>
+	);
+}
