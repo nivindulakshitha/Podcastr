819a5023179da75eec14e3260c37a8b3fb429ae0
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-11 10:09:04 +0530

    Add "convex" package dependency and integrate Convex into the app

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -7,7 +7,11 @@ const PodcastCard = ({podcast} : {podcast: any}) => {
 	return (
 		<div className='cursor-pointer '>
 			<figure className="flex flex-col gap-2">
-				<Image src={imgURL} alt={title} width={175} height={175} />
+				<Image src={imgURL} alt={title} width={175} height={175} className='aspect-square h-fit w-full rounded-xl 2xl:size-[200px]' />
+				<div className="flex flex-col">
+					<h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
+					<h2 className="text-12 truncate font-normal capitalize text-white-4">{ description }</h2>
+					</div>
 			</figure>
 		</div>
   )
--- constants/index.ts
+++ constants/index.ts
@@ -49,55 +49,55 @@ export const podcastData = [
 		title: "The Joe Rogan Experience",
 		description: "A long form, in-depth conversation",
 		imgURL:
-			"https://lovely-flamingo-139.convex.cloud/api/storage/3106b884-548d-4ba0-a179-785901f69806",
+			"https://placehold.co/400x400/555555/FFFFFF.png",
 	},
 	{
 		id: 2,
 		title: "The Futur",
 		description: "This is how the news should sound",
 		imgURL:
-			"https://lovely-flamingo-139.convex.cloud/api/storage/16fbf9bd-d800-42bc-ac95-d5a586447bf6",
+			"https://placehold.co/400x400/555555/FFFFFF.png",
 	},
 	{
 		id: 3,
 		title: "Waveform",
 		description: "Join Michelle Obama in conversation",
 		imgURL:
-			"https://lovely-flamingo-139.convex.cloud/api/storage/60f0c1d9-f2ac-4a96-9178-f01d78fa3733",
+			"https://placehold.co/400x400/555555/FFFFFF.png",
 	},
 	{
 		id: 4,
 		title: "The Tech Talks Daily Podcast",
 		description: "This is how the news should sound",
 		imgURL:
-			"https://lovely-flamingo-139.convex.cloud/api/storage/5ba7ed1b-88b4-4c32-8d71-270f1c502445",
+			"https://placehold.co/400x400/555555/FFFFFF.png",
 	},
 	{
 		id: 5,
 		title: "GaryVee Audio Experience",
 		description: "A long form, in-depth conversation",
 		imgURL:
-			"https://lovely-flamingo-139.convex.cloud/api/storage/ca7cb1a6-4919-4b2c-a73e-279a79ac6d23",
+			"https://placehold.co/400x400/555555/FFFFFF.png",
 	},
 	{
 		id: 6,
 		title: "Syntax ",
 		description: "Join Michelle Obama in conversation",
 		imgURL:
-			"https://lovely-flamingo-139.convex.cloud/api/storage/b8ea40c7-aafb-401a-9129-73c515a73ab5",
+			"https://placehold.co/400x400/555555/FFFFFF.png",
 	},
 	{
 		id: 7,
 		title: "IMPAULSIVE",
 		description: "A long form, in-depth conversation",
 		imgURL:
-			"https://lovely-flamingo-139.convex.cloud/api/storage/8a55d662-fe3f-4bcf-b78b-3b2f3d3def5c",
+			"https://placehold.co/400x400/555555/FFFFFF.png",
 	},
 	{
 		id: 8,
 		title: "Ted Tech",
 		description: "This is how the news should sound",
 		imgURL:
-			"https://lovely-flamingo-139.convex.cloud/api/storage/221ee4bd-435f-42c3-8e98-4a001e0d806e",
+			"https://placehold.co/400x400/555555/FFFFFF.png",
 	},
 ];
