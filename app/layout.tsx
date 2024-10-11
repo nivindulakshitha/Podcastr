756c84acc0cd8dedaa6f5b197ed7dab0be96bdaa
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-11 15:01:13 +0530

    Refactor ConvexClientProvider to ConvexClerkProvider in app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ConvexClientProvider } from "./providers/ConvexProvider";
+import ConvexClerkProvider from "./providers/ConvexClerkProvider";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -32,7 +32,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<ConvexClientProvider>{children}</ConvexClientProvider>
+				<ConvexClerkProvider>{children}</ConvexClerkProvider>
 			</body>
 		</html>
 	);
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as http from "../http.js";
 import type * as users from "../users.js";
 
 /**
@@ -26,6 +27,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  http: typeof http;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
