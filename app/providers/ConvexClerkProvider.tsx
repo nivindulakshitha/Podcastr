d9d106e8924d5f0925b95316b07940bf5ba1b968
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2024-11-15 11:04:55 +0530

    Enhance ClerkProvider appearance with custom styles and logo

--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -7,7 +7,19 @@ import { ConvexReactClient } from "convex/react";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
-	<ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+	<ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string} appearance={{
+		layout: { socialButtonsVariant: 'iconButton', logoImageUrl: '/icons/logo-auth.svg', },
+		variables: {
+			colorBackground: '#15171C',
+			colorPrimary: '#F9F9F9',
+			colorText: '#F9F9F9',
+			colorInputBackground: '#1B1F29',
+			colorInputText: '#F9F9F9',
+		}
+		
+
+	}
+	}>
 		<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
 			{children}
 		</ConvexProviderWithClerk>
