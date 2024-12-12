b1c5fe3ac40bcec33deacef5e7f8a2857a45c8b8
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2024-12-12 21:11:31 +0530

    Add file handling API with generateUploadUrl mutation

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as files from "../files.js";
 import type * as http from "../http.js";
 import type * as openai from "../openai.js";
 import type * as users from "../users.js";
@@ -28,6 +29,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  files: typeof files;
   http: typeof http;
   openai: typeof openai;
   users: typeof users;
